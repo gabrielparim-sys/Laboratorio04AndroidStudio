58c0c7849ffb769c7974a0565e7c58547ee52a61
gabrielparim-sys <gabriel.pari.m@tecsup.edu.pe>
2025-09-21 22:37:28 -0500

    feat: Modifacion a Addmovie

--- app/src/main/java/com/example/lab04/ui/theme/AddMovieFAB.kt
+++ app/src/main/java/com/example/lab04/ui/theme/AddMovieFAB.kt
@@ -4,11 +4,20 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun AddMovieFAB(onClick: () -> Unit) {
-    FloatingActionButton(onClick = onClick) {
-        Icon(Icons.Filled.Add, contentDescription = "Agregar película")
+    FloatingActionButton(
+        onClick = onClick,
+        containerColor = MaterialTheme.colorScheme.primary, // 🎨 Color más visible
+        contentColor = Color.White // 🎨 Ícono en blanco
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Add,
+            contentDescription = "Agregar película"
+        )
     }
 }
